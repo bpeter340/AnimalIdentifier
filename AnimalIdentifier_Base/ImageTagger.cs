323995cb14727a7d5bf4bbdbd01d60f6ddc3175c
Eric Potter <pottereric@hotmail.com>
2018-02-07 22:32:05 -0500

    Get the tags for an image.

---
+++ AnimalIdentifier_Base/ImageTagger.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalIdentifier_Base
+{
+    public class ImageTagger
+    {
+        const string subscriptionKey = "3a1afe82e29844138e8e8915e006581b";
+        const string uriBase = "https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+
+        public List<Tag> GetTagsForImage(string imagePath)
+        {
+            return MakeAnalysisRequest(imagePath).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file.</param>
+        static async Task<List<Tag>> MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "visualFeatures=Tags&language=en";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                //Console.WriteLine((contentString));
+                var tagData = JsonConvert.DeserializeObject<TagData>(contentString);
+
+                return tagData.tags;
+            }
+
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
--- AnimalIdentifier_Base/Program.cs
+++ AnimalIdentifier_Base/Program.cs
@@ -8,8 +8,24 @@ namespace AnimalIdentifier_Base
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Analyze an image:");
+            Console.Write("Enter the path to an image you wish to analzye: ");
+            string imageFilePath = Console.ReadLine();
+
+            ImageTagger animalImageTagger = new ImageTagger();
+            List<Tag> tags = animalImageTagger.GetTagsForImage(imageFilePath);
+
+            foreach (Tag tag in tags)
+            {
+                Console.WriteLine(tag.name);
+            }
+
+            Console.WriteLine("Press Any Key to Continue.");
+            Console.ReadLine();
         }
     }
 }
